8e603b0bf219689bec12a74c4136305c3493a091
poi233 <593657039@qq.com>
2018-01-01 20:15:45 +0800

    复原误删的文件

---
+++ app/src/main/java/com/qxkqyy/activity/doctor_result.kt
@@ -0,0 +1,7 @@
+package com.qxkqyy.activity
+
+/**
+ * Created by puyihao on 2018/1/1.
+ */
+data class doctor_result (val doctor: Doctor)
+data class Doctor(val Name:String,val Good:String,val PicURL:String)
